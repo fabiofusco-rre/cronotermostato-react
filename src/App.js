242e147812afaa4d5daafd9d01d85664e2114b89
Peppe Lauro <peppe.lauro@gmail.com>
2022-09-07 10:42:37 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,254 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect }  from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Dashboard from './components/Dashboard'
+import ConfigPanel from './components/ConfigPanel'
+  
+export const ConfContext = React.createContext("conf");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    // const [users, setUsers] = useState([]);
+    const [tabIndex, setTabIndex] = useState('1');
+    const [settingsOpen, setSettingsOpen] = useState(false);
+    const [zones, setZones] = useState([]);
+    const [climateSensors, setClimateSensors] = useState([])
+    const [temperatureSensors, setTemperatureSensors] = useState([])
+    const [appConfig, setAppConfig] = useState([])
+
+    const handleSettingsOpen = () => {
+       setSettingsOpen(!settingsOpen);
+    };   
+
+    const handleTabChange = (event, newValue) => {    
+        console.log(newValue)    
+        setTabIndex(newValue);
+        
+        appConfig.currentTab = newValue - 1
+        setAppConfig(appConfig)
+    };
+
+    const defaultHeader = {
+        headers: {
+          'Authorization': localStorage.getItem("haApiToken") || "ABCDEFG"
+        },
+    };
+
+    useEffect(() => {     
+
+        //Far diventare API???
+        setZones([        
+            { key: "1", label: "Giorno" },
+            { key: "2", label: "Notte" },            
+        ])
+
+        // DA RECUPERARE VIA API
+        //setClimateSensors([])
+
+        // DA RECUPERARE VIA API
+        //setTemperatureSensors([])
+
+        const urlGetClimateSensors = localStorage.getItem("urlGetClimateSensors") || "https://jsonplaceholder.typicode.com/users/";
+        fetch(urlGetClimateSensors, defaultHeader)
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    //setIsLoaded(true);
+                    const r = data.filter(el => el.entity_id.startsWith("climate."));
+                    console.log('r',r)
+                    setClimateSensors(r.map(i => i.entity_id));
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+        
+        const urlGetTemperatureSensors = localStorage.getItem("urlGetTemperatureSensors") || "https://jsonplaceholder.typicode.com/users/";
+        fetch(urlGetTemperatureSensors, defaultHeader)
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    //setIsLoaded(true);                    
+                    const r = data.filter(el => el.entity_id.startsWith("temperature."));
+                    console.log('r',r)
+                    setTemperatureSensors(r.map(i => i.entity_id));      
+                    setIsLoaded(true)              
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )        
+
+        let conf = null
+        const saved = localStorage.getItem("appConfig");
+        if(saved) {
+            conf = JSON.parse(saved)
+        } else {        
+            conf = {zones: []}
+            for(const z of zones) {
+                conf.zones.push({
+                    ...z, 
+                    setpointDefault: [
+                        {label: 'Eco', value: 16 + Math.floor(Math.random() * 10)}, 
+                        {label: 'Confort', value: 18 + Math.floor(Math.random() * 10)}, 
+                        {label: 'Non in casa', value: 12},
+                    ],
+                    setpointTimeslice: {
+                        '00:00' : 0,
+                        '00:30' : 0,                    
+                        '01:00' : 0,
+                        '01:30' : 0,
+                        '02:00' : 0,
+                        '02:30' : 0,
+                        '03:00' : 0,
+                        '03:30' : 0,
+                        '04:00' : 0,
+                        '04:30' : 0,
+                        '05:00' : 0,
+                        '05:30' : 0,
+                        '06:00' : 0,
+                        '06:30' : 0,
+                        '07:00' : 0,
+                        '07:30' : 0,
+                        '08:00' : 0,
+                        '08:30' : 0,
+                        '09:00' : 0,
+                        '09:30' : 0,
+                        '10:00' : 0,
+                        '10:30' : 0,
+                        '11:00' : 0,
+                        '11:30' : 0,
+                        '12:00' : 0,
+                        '12:30' : 0,
+                        '13:00' : 0,
+                        '13:30' : 0,
+                        '14:00' : 0,
+                        '14:30' : 0,
+                        '15:00' : 0,
+                        '15:30' : 0,
+                        '16:00' : 0,
+                        '16:30' : 0,
+                        '17:00' : 0,
+                        '17:30' : 0,
+                        '18:00' : 0,
+                        '18:30' : 0,
+                        '19:00' : 0,
+                        '19:30' : 0,
+                        '20:00' : 0,
+                        '20:30' : 0,
+                        '21:00' : 0,
+                        '21:30' : 0,
+                        '22:00' : 0,
+                        '22:30' : 0,
+                        '23:00' : 0,
+                        '23:30' : 0,
+                    },
+                    climateSensors: [],
+                    temperatureSensor: ''
+                })
+            }
+            conf.currentTab = 0
+            conf.status = false
+            conf.vacationMode = false
+            conf.vacationTemperature = 14
+            conf.climateSensors = climateSensors
+            conf.temperatureSensors = temperatureSensors
+
+            //console.log(conf)
+            
+        }
+        setAppConfig(conf)
+        
+        console.log('initial',conf)
+        //localStorage.setItem("appConfig", JSON.stringify(conf));
+        //if(temperatureSensors.length > 0) {
+        //setIsLoaded(true)
+        //}
+      }, [isLoaded])
+    
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        console.log(appConfig)
+        return (            
+                <div>
+                    <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <AppBar position="static">
+                                <Toolbar>                            
+                                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                        Cronoterm Manager
+                                    </Typography>
+                                    <IconButton
+                                        size="large"
+                                        edge="start"
+                                        color="inherit"
+                                        aria-label="settings"
+                                        sx={{ mr: 2 }}
+                                        onClick={handleSettingsOpen}
+                                    >                                 
+                                        <SettingsIcon />
+                                    </IconButton>                   
+                                </Toolbar>
+                            </AppBar>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TabContext value={tabIndex}>
+                                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                <TabList onChange={handleTabChange} aria-label="tab">
+                                {appConfig.zones.map((data) => {
+                                    return (
+                                        <Tab label={data.label} value={data.key} key={data.key} />                            
+                                    );
+                                })}                               
+                                </TabList>
+                                </Box>
+                                {appConfig.zones.map((data) => {                                
+                                    return (
+                                        <TabPanel value={data.key} key={data.key}>
+                                            <Dashboard name={data.label} climateSensors={climateSensors} temperatureSensors={temperatureSensors} appConfig={appConfig} setAppConfig={setAppConfig} /> 
+                                        </TabPanel>                                
+                                    );
+                                })}                            
+                            </TabContext>  
+                        </Grid>
+                        
+                        {/*<Grid item xs={12}>
+                            <ul>
+                            {users.map(user => (
+                            <li key={user.id}>
+                                {user.name} 
+                            </li>
+                            ))}
+                            </ul>
+                            </Grid>*/}
+                        
+
+                        <Grid item xs={12}>
+                        
+                        </Grid>
+                    </Grid>
+                    </Box>
+                                    
+                    <ConfigPanel appConfig={appConfig} setAppConfig={setAppConfig} open={settingsOpen} toggleOpen={handleSettingsOpen} /> 
+                    
+                </div>            
+        );
+    }
 }
-
 export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,216 @@
+import React, { useState, useEffect }  from 'react';
+import Grid from '@mui/material/Grid';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import TemperatureSlider from './TemperatureSlider'
+import MultipleSelectChip from './MultipleSelectChip'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import ConfigTimeslice from './ConfigTimeslice'
+import EditIcon from '@mui/icons-material/Edit';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf';
+import WeekendIcon from '@mui/icons-material/Weekend';
+import ForestIcon from '@mui/icons-material/Forest';
+import NaturePeopleIcon from '@mui/icons-material/NaturePeople';
+
+let marks = []
+let label = ''
+for(let i = 5; i <= 35; i++) {
+    label = i % 5 === 0 ? i+'°C' : ''
+    marks.push({value: i, label: label})
+}
+
+let timeslice = []
+let h = ''
+for(let i = 0; i <= 23; i++) {
+    h = i.toString().padStart(2, '0')
+    timeslice.push(h + ':00')
+    timeslice.push(h + ':30')
+}
+
+const setpointIcon = {
+    0: <EnergySavingsLeafIcon />,
+    1: <WeekendIcon />,
+    2: <NaturePeopleIcon />
+}
+
+const setpointLegend = {
+    0: "Eco",
+    1: "Confort",
+    2: "Non in casa"
+}
+
+  
+const Dashboard = ({name, climateSensors, temperatureSensors, appConfig, setAppConfig}) => {    
+    const [isLoaded, setIsLoaded] = useState(false)    
+    const [temperatureSensor, setTemperatureSensor] = useState('')
+    const [tabValue, setTabValue] = useState('00:00')
+    const [settingsOpen, setSettingsOpen] = useState(false);
+
+    useEffect(() => {
+        // const saved = localStorage.getItem("appConfigig");
+        // const initialValue = JSON.parse(saved);
+        
+        setTemperatureSensor(appConfig.zones[appConfig.currentTab].temperatureSensor)
+        // setTemperatureSensor(
+        setIsLoaded(true)
+    }, [isLoaded])
+
+    const handleSettingsOpen = () => {        
+        setSettingsOpen(!settingsOpen);
+     };   
+
+    const temperatureSensorChange = (event) => {
+        const prev = temperatureSensor
+        const curr = event.target.value
+
+        setTemperatureSensor(curr);
+
+        let conf = {...appConfig}
+        conf.zones[appConfig.currentTab].temperatureSensor = event.target.value        
+
+        //Rimuove elemento dalla lista perché impegnato
+        const index = conf.temperatureSensors.indexOf(curr);
+        if (index > -1) {
+            conf.temperatureSensors.splice(index, 1);            
+        }
+        
+        if(prev.length > 0) {
+            conf.temperatureSensors.push(prev)
+        }        
+        
+        //Elimina i duplicati
+        const set = new Set(conf.temperatureSensors);        
+        conf.temperatureSensors = [...set]
+
+        temperatureSensors = conf.temperatureSensors
+
+        localStorage.setItem("appConfig", JSON.stringify(conf));
+
+        setAppConfig(conf)
+        console.log(conf)
+    };
+
+    const tabChange = (event, newValue) => {        
+        setTabValue(newValue);
+    };
+    
+    const handleTemperatureChange = (event, newValue) => {            
+        const name = event.target.name
+        //console.log(newValue, name, appConfig)          
+        let conf = {...appConfig}
+        conf.zones[appConfig.currentTab].setpointDefault[name].value = newValue        
+        localStorage.setItem("appConfig", JSON.stringify(conf));
+        setAppConfig(conf)
+        
+    };
+
+    // console.log('appConfigig:', appConfig)
+    return (isLoaded &&
+        <div>
+            <h1>Dashboard {name}</h1>
+            <h2>Set point per ciascuna mezz'ora:</h2>
+            <Box sx={{ bgcolor: 'background.paper' }}>
+            <Tabs      
+                value={tabValue}          
+                onChange={tabChange}
+                variant="scrollable"
+                scrollButtons="auto"
+                aria-label="scrollable auto tabs example"                
+            >
+                {timeslice.map((h) => {
+                    const setpoint = setpointLegend[appConfig.zones[appConfig.currentTab].setpointTimeslice[h]]    
+                    const setpointDegree = appConfig.zones[appConfig.currentTab].setpointDefault.filter(obj => {return obj.label === setpoint})[0].value+'°'
+                    const icon = setpointIcon[appConfig.zones[appConfig.currentTab].setpointTimeslice[h]]                    
+                    return <Tab disabled={appConfig.vacationMode} icon={<span>{icon}<span><br></br>{setpoint}<br></br>{setpointDegree}</span></span>} iconPosition="bottom" label={h} wrapped key={h} value={h} onClick={handleSettingsOpen} />
+                })}                
+            </Tabs>
+            </Box>
+          
+            <h2>Sensori ambiente:</h2>
+            <Grid container rowSpacing={6} spacing={2} >
+                <Grid item xs={2}>
+                    Testine termostatiche:
+                </Grid>
+                <Grid item xs={10}>
+                    {!appConfig.vacationMode && (<MultipleSelectChip items={climateSensors} />)}
+                </Grid>
+                <Grid item xs={2}>
+                    Sensore temperatura:
+                </Grid>
+                <Grid item xs={10}>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={temperatureSensor}                    
+                    onChange={temperatureSensorChange}
+                    disabled={appConfig.vacationMode}
+                    >                   
+                    {temperatureSensors.map((name) => (
+                        <MenuItem
+                        key={name}
+                        value={name}                        
+                        >
+                        {name}
+                        </MenuItem>
+                    ))}
+                    </Select>
+                </Grid>
+            </Grid>
+            <h2>Set point ambiente:</h2>
+            <Grid container rowSpacing={6} spacing={2}>
+                <Grid item xs={2}>
+                    <span><EnergySavingsLeafIcon /></span>
+                    <br></br>
+                    Eco:
+                </Grid>
+                <Grid item xs={10}>                
+                    <TemperatureSlider 
+                        name={"0"} 
+                        min={4} 
+                        max={35} 
+                        value={appConfig.zones[appConfig.currentTab].setpointDefault[0].value} 
+                        onValueChange={handleTemperatureChange} 
+                        disabled={appConfig.vacationMode} />
+                </Grid>
+
+                <Grid item xs={2}>
+                    <span><WeekendIcon /></span>
+                    <br></br>
+                    Confort:
+                </Grid>
+                <Grid item xs={10}>
+                    <TemperatureSlider 
+                        name={"1"} 
+                        min={4} 
+                        max={35}
+                        value={appConfig.zones[appConfig.currentTab].setpointDefault[1].value} 
+                        onValueChange={handleTemperatureChange} 
+                        disabled={appConfig.vacationMode} />
+                </Grid>
+
+                <Grid item xs={2}>
+                    <span><NaturePeopleIcon /></span>
+                    <br></br>
+                    Non in casa:
+                </Grid>
+                <Grid item xs={10}>
+                    <TemperatureSlider 
+                        name={"2"} 
+                        min={4} 
+                        max={35} 
+                        value={appConfig.zones[appConfig.currentTab].setpointDefault[2].value} 
+                        onValueChange={handleTemperatureChange} 
+                        disabled={appConfig.vacationMode} />
+                </Grid>
+            </Grid>
+
+            <ConfigTimeslice open={settingsOpen} toggleOpen={handleSettingsOpen} title={tabValue} appConfig={appConfig} setAppConfig={setAppConfig} />             
+        </div>
+    )
+    
+}
+
+export default Dashboard;
